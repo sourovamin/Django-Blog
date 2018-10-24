d3ce56692489fbe05db8cbac9fe72ad330f95d48
Sourov Amin <sourov.amin@gmail.com>
2018-10-24 10:54:44 +0600

    Login Added

---
+++ users/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class UserRegisterForm(UserCreationForm):
+
+    email = forms.EmailField()
+    firstName = forms.CharField(label='First Name', required= False)
+    lastName = forms.CharField(label='First Name', required=False)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'firstName','lastName', 'password1', 'password2']
--- users/views.py
+++ users/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'New User {username} is Created, Login Now!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
